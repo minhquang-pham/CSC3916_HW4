47542ca5576b9654f0f8ef5d9ff930156cb4154c
minhquang-pham <minhquang.pham@ucdenver.edu>
2021-03-28 21:24:56 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
 var Movie = require('./Movies');
+var Review = require('./Reviews');
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());
